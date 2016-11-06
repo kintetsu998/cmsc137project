20ba5a84d1fca6e91ec891587714e2099594eec7
lmlawas1 <lmlawas@up.edu.ph>
2016-11-06 11:21:02 +0800

    Add working UI for chat

--- src/carwars/chat/ChatRoom.java
+++ src/carwars/chat/ChatRoom.java
@@ -3,6 +3,7 @@ package carwars.chat;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import carwars.init.PlayerLogin;
 
 public class ChatRoom extends JFrame implements ActionListener {
 
@@ -12,12 +13,15 @@ public class ChatRoom extends JFrame implements ActionListener {
     private JTextArea ta;
     private int defaultPort;
     private String defaultHost;
+    private Client defaultClient;
 
-    public ChatRoom(String host, int port, String username) {
+    public ChatRoom(String host, int port, String username){//, Client client) {
 
         super("Car Wars - " + username);
         defaultPort = port;
         defaultHost = host;
+        defaultClient = new Client(username, host, port, this);
+        defaultClient.connect();
         
         JPanel northPanel = new JPanel(new GridLayout(3,1));
         JPanel serverAndPort = new JPanel(new GridLayout(1,5, 1, 3));
@@ -39,11 +43,22 @@ public class ChatRoom extends JFrame implements ActionListener {
         tfMsg = new JTextField("");
         tfMsg.setBackground(Color.WHITE);
         tfMsg.setForeground(Color.BLACK);
+        tfMsg.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+
+                defaultClient.sendMessage(tfMsg.getText().trim());
+                tfMsg.setText("");
+                // JOptionPane.showMessageDialog(null , "You've Submitted the name " + nameInput.getText());
+
+            }
+        });
         northPanel.add(tfMsg);
         add(northPanel, BorderLayout.NORTH);
 
-        // The CenterPanel which is the chat room
+        ta = new JTextArea("", 80, 80);
         JPanel centerPanel = new JPanel(new GridLayout(1,1));
+        centerPanel.add(new JScrollPane(ta));
+        ta.setEditable(false);
         add(centerPanel, BorderLayout.CENTER);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -51,26 +66,22 @@ public class ChatRoom extends JFrame implements ActionListener {
         setVisible(true);
     }
 
-    // to be removed; added for test purposes
     public static void main(String[] args){
         ChatRoom cr = new ChatRoom("127.0.0.1", 8080, "Leensey");
     }
 
-    // called by the Client to append text in the TextArea 
     void append(String str) {
         ta.append(str);
         ta.setCaretPosition(ta.getText().length() - 1);
     }
 
-    public void actionPerformed(ActionEvent e) {
-
-        // ok it is coming from the JTextField
-        if(true) {
-            // just have to send the message
-            // client.sendMessage(new ChatMessage(ChatMessage.MESSAGE, tf.getText()));             
-            // tf.setText("");
+    public void actionPerformed(ActionEvent ev) {
+        if(ev.getSource() == tfMsg) {
+            defaultClient.sendMessage(tfMsg.getText().trim());
+            tfMsg.setText("");
             return;
         }
+        
     }
 
 }
--- src/carwars/chat/Client.java
+++ src/carwars/chat/Client.java
@@ -4,93 +4,116 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import carwars.init.PlayerLogin;
+
 import java.net.*;
-import java.util.Scanner;
 
 public class Client {
-	private String name;
+    
+    private DataInputStream in;
+    private DataOutputStream out;
+    private int port;
+    private String name;
+    private String server;
+    private Socket sock;
+    private ChatRoom gui;
 
-	public Client(String name) {
-		this.name = name;
-	}
+    public Client(String name, String server, int port, ChatRoom gui) {
+        this.name = name;
+        this.server = server;
+        this.port = port;
+        this.gui = gui;
+    }
 
-	public void connect(String server, int port) {
-		try {
-			DataOutputStream out;
-			DataInputStream in;
-			
-			Scanner sc = new Scanner(System.in);
-			Socket client = new Socket(server, port);
-			String message;
+    public boolean connect() {
+        // open socket connection
+        try {
+            sock = new Socket(server, port);
+            
+            // open input and output streams
+            try{
+                in  = new DataInputStream(sock.getInputStream());
+                out = new DataOutputStream(sock.getOutputStream());
+            }
+            catch (IOException e1) {
+                display("Exception creating new I/O streams: " + e1);
+                return false;
+            }
 
-			System.out.println("Connected to " + client.getRemoteSocketAddress());
+            out.writeUTF(this.name);
 
-			out = new DataOutputStream(client.getOutputStream());
-			out.writeUTF(this.name);
+            // receive messages
+            new Thread() {
+                public void run() {
+                    try{
+                        String reply;
+                        while(!sock.isClosed()){
+                            while((reply = in.readUTF()) == null){}
+                            display(reply);
+                        }
+                    } catch(Exception e1) {
+                        display("Exception in thread: " + e1);
+                    }
+                }
+            }.start();
 
-			new Thread() {
-				@Override
-				public void run() {
-					try{
-						DataInputStream in = new DataInputStream(client.getInputStream());
-						String reply;
-						while(!client.isClosed()){
-							while((reply = in.readUTF()) == null){}
-							System.out.println(reply);
-						}
-					} catch(Exception e) {
-						//e.printStackTrace();
-						return;
-					}
-				}
-			}.start();
+        } catch (ConnectException e2) {
+            display("Exception connecting to server: " + e2);
+            System.out.println(e2);
+            return false;
+        } catch (IOException e3) {
+            // e.printStackTrace();
+            display("Exception in server/port I/O: " + e3);
+            return false;
+        }
+        return true;
+    }
 
-			do{
-				message = sc.nextLine().trim();
-				out.writeUTF(message);
-			} while(!message.equalsIgnoreCase("exit"));
+    public void sendMessage(String message){
+        try {
+            out.writeUTF(message);
+            display(name + ": " + message);
+        }
+        catch(IOException e) {
+            display("Exception sending to server: " + e);
+        }
+    }
 
-			client.close();
-		} catch (ConnectException e) {
-		    System.out.println(e);
-		    return;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return;
-		}
-	}
+    public String getName(){
+        return this.name;
+    }
 
-	public String getName(){
-		return this.name;
-	}
+    // public static void main(String[] args) {
+    //     Scanner sc = new Scanner(System.in);
+    //     String serverName;
+    //     String name;
+    //     int port;
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String serverName;
-		String name;
-		int port;
+    //     if(args.length < 2) {
+    //         help();
+    //         return;
+    //     }
 
-		if(args.length < 2) {
-			help();
-			return;
-		}
+    //     serverName = args[0];
+    //     try{
+    //         port = Integer.parseInt(args[1]);
+    //     } catch (NumberFormatException e) {
+    //         help();
+    //         return;
+    //     }
 
-		serverName = args[0];
-		try{
-			port = Integer.parseInt(args[1]);
-		} catch (NumberFormatException e) {
-			help();
-			return;
-		}
+    //     System.out.print("Enter your name: ");
+    //     name = sc.nextLine();
+        
+    //     new Client(name).connect(serverName, port); 
+    // }
 
-		System.out.print("Enter your name: ");
-		name = sc.nextLine();
-		
-		new Client(name).connect(serverName, port);	
-	}
+    public void display(String msg) {
+        gui.append(msg + "\n");      // append to the PlayerLogin JTextArea (or whatever)
+    }
 
-	private static void help() {
-		System.out.println("Using the Client class:");
-		System.out.println("java Client [ip address] [port number]");
-	}
+    private static void help() {
+        System.out.println("Using the Client class:");
+        System.out.println("java Client [ip address] [port number]");
+    }
 }
